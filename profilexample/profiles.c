b601041539a2b1c68d3d6dac8d23ac3480003d58
CrunchwrapSP <ew.colsch@gmail.com>
2024-12-02 14:24:56 -0600

    Add files via upload

--- profilexample/profiles.c
+++ profilexample/profiles.c
@@ -3,61 +3,33 @@
 #include <stdlib.h>
 
 int main() {
-	
-	char name1[] = "Emmett";
-	char name2[] = "Collin";
-	char name3[] = "Steve";
-	
-	int zip1 = 52332;
-	int zip2 = 52349;
-	int zip3 = 50012;
-	
-	char choices1[] = "nnnn";
-	char choices2[] = "nnnn";
-	char choices3[] = "nnnn";
+	//Create flag for loop and struct for profiles
+	char display = 'n';
 	
 	struct Profile {
-    char username[50];
+    char name[50];
     int zipCode;
-    char choice;
+    char choice[3];
 	};
 	
 	
 	FILE *fptr;
 
-	// Open a file in writing mode
-	fptr = fopen("example.txt", "w");
-
-	// Write some text to the file
-	fprintf(fptr, "%s %d %s\n", name1, zip1, choices1);
-	fprintf(fptr, "%s %d %s\n", name2, zip2, choices2);
-	fprintf(fptr, "%s %d %s\n", name3, zip3, choices3);
-
-	// Close the file
-	fclose(fptr);
-	
-	
-	
-// Define a structure to hold profile information
-
-
-
-    FILE *file;
-    struct Profile profiles[10];  // Array to store up to 10 profiles
+    struct Profile profiles[3];  // Array to store up to 3 profiles
     int profileCount = 0;         // To keep track of how many profiles were read
-
-    // Open the file in read mode
-    file = fopen("example.txt", "r");
-    if (file == NULL) {
+	
+	
+	fptr = fopen("example.txt", "r");
+    if (fptr == NULL) {
         // If the file cannot be opened, print an error message and exit
         perror("Error opening file");
         return 1;
     }
 
     // Read the profiles until the end of the file or the array limit is reached
-    while (fscanf(file, "%s %d %s", profiles[profileCount].username, 
+    while (fscanf(fptr, "%s %d %s", profiles[profileCount].name, 
                    &profiles[profileCount].zipCode, 
-                   &profiles[profileCount].choice) == 3) {
+                   profiles[profileCount].choice) == 3) {
         profileCount++;
 
         // If we exceed the array limit, stop reading further
@@ -68,16 +40,123 @@ int main() {
     }
 
     // Close the file after reading
-    fclose(file);
+    fclose(fptr);
+	
+	fptr = fopen("example.txt", "w");		
+	// Write some text to the file
+	
+	fprintf(fptr, "%s %d %s\n", profiles[0].name, profiles[0].zipCode, profiles[0].choice);
+	fprintf(fptr, "%s %d %s\n", profiles[1].name, profiles[1].zipCode, profiles[1].choice);
+	fprintf(fptr, "%s %d %s\n", profiles[2].name, profiles[2].zipCode, profiles[2].choice);
+	
 
-    // Print the profiles that were read
-    printf("Profiles read from file:\n");
-    for (int i = 0; i < profileCount; i++) {
-        printf("Profile #%d\n", i + 1);
-        printf("  Username: %s\n", profiles[i].username);
-        printf("  Zip Code: %d\n", profiles[i].zipCode);
-        printf("  Other Integer: %s\n\n", profiles[i].choice);
-    }
+	// Close the file
+	fclose(fptr);
+	
+	printf("Hello Welcome to the Weather App\n");
+	while(display != 'y') {
+		//initialize local variables
+		int initchoice = -1;
+		int currpro = -1;
+		char usernamestring[20];
+		
+		char new = 'p';
+		
+		//ask question to go into switch funtcion
+		printf("Would you like to display weather(1), change/add weather profiles(2), or see a guide to our app(3)\n");
+		
+		while(initchoice < 1 || initchoice > 3) {
+			scanf(" %d", &initchoice);
+			if(initchoice < 1 || initchoice > 3)
+				printf("That is not one of your choices, choose 1, 2, or 3.\n");
+		}
+		
+		switch(initchoice){
+			case(1):
+				//check if any available profiles to display
+				if(profiles[0].name[0] == '?' && profiles[1].name[0] == '?' && profiles[2].name[0] == '?') {
+					printf("No old profiles\n");
+					break;
+				}
+				//choose which one to display
+				printf("Which profile would you like to display? %s, %s, %s(1,2,3)\n", profiles[0].name, profiles[1].name, profiles[2].name);
+				while(currpro < 1|| currpro > 3) { 
+					scanf("%d", &currpro);
+					if(currpro > 0 && currpro < 4)
+						break;
+					printf("That is not a profile. Try Again.\n");
+				}
+				//print profile values
+				printf("Profile : %s \n", profiles[currpro-1].name);
+				printf("Zip code : %d\n", profiles[currpro-1].zipCode);
+				printf("Humidity : %c\n", profiles[currpro-1].choice[0]);
+				printf("Wind speed : %c\n", profiles[currpro-1].choice[1]);
+				printf("Cloud Coverage : %c\n", profiles[currpro-1].choice[2]);
+				
+				
+				//ask if they want to go into ncurses display
+				printf("Would you like to see this info?(y/n)\n");
+				scanf(" %c", &display);
+				break;
+			case(2):
+				// ask if they want to delete or create profile
+				printf("Would you like to add a new profile or delete an old one?(n/d)\n");
+				scanf(" %c", &new);
+				//choose which profile slot to put new info in
+				if(new == 'n'){
+					if(profiles[0].name[0] == '?')
+						currpro = 1;
+					else if(profiles[1].name[0] == '?')
+						currpro = 2;
+					else if(profiles[2].name[0] == '?')
+						currpro = 3;
+					else {
+						printf("Profiles full erase one first");
+						break;
+					}
+					printf("%d", currpro);
+					//ask and assign relative values based on user input for new values
+					printf("What would you like your profile name to be?\n");
+					scanf("%s", usernamestring);
+					strcpy(profiles[currpro-1].name, usernamestring);
+					printf("What is the zip code of preferred weather?\n");
+					scanf("%d", &profiles[currpro-1].zipCode);
+					printf("Would you like to display humidity?(y/n)\n");
+					scanf(" %c", &profiles[currpro-1].choice[0]);
+					printf("Would you like to display wind speed?(y/n)\n");
+					scanf(" %c", &profiles[currpro-1].choice[1]);
+					printf("Would you like to display cloud coverage?(y/n)\n");
+					scanf(" %c", &profiles[currpro-1].choice[2]);
+					
+				}
+				if(new == 'd'){
+					//change profile name to ? to "delete"
+					printf("Which profile would you like to delete? %s, %s, %s(1,2,3)\n", profiles[0].name, profiles[1].name, profiles[2].name);
+					scanf("%d", &currpro);
+					if(currpro == 1)
+						strcpy(profiles[0].name, "?");
+					if(currpro == 2)
+						strcpy(profiles[1].name, "?");
+					if(currpro == 3)
+						strcpy(profiles[2].name, "?");
+				}
+				break;
+			case(3):
+				printf("HOW TO\n");
+				break;
+		}
+			// Open profile file in writing mode
+		fptr = fopen("example.txt", "w");
+		
+		// Write profiles to the file
+		fprintf(fptr, "%s %d %s\n", profiles[0].name, profiles[0].zipCode, profiles[0].choice);
+		fprintf(fptr, "%s %d %s\n", profiles[1].name, profiles[1].zipCode, profiles[1].choice);
+		fprintf(fptr, "%s %d %s\n", profiles[2].name, profiles[2].zipCode, profiles[2].choice);
 
-    return 0;
+		// Close the file
+		fclose(fptr);
+		
+	
+	}
+    
 }
